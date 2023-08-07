f3d11419a3367df833ed06ac38896fe04f510a7d
alsomeb <alsomeb@gmail.com>
2023-08-07 14:14:40 +0200

    csrf token practice

--- src/main/java/com/example/learn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/learn/springsecurity/config/SecurityConfig.java
@@ -7,8 +7,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
+   The OPTIONS request is a preflight request made by the browser
+   to check whether the actual request is safe to send or not.
+
+   By permitting all OPTIONS requests, you're allowing the browser to perform
+   the necessary CORS checks before making the actual request, which helps to avoid the CORS error on client.
+
+   We need to make a CORS config as well.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -20,8 +30,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .anyRequest()
                         .authenticated())
+                // Disable Http Session -> Making REST API Stateless
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Disable CSRF
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults()) // Basic Auth enabled
+                // Basic Auth enabled with default Pop Up Modal for Login Credentials in Browser
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 }
--- src/main/java/com/example/learn/springsecurity/controllers/HelloWorldController.java
+++ src/main/java/com/example/learn/springsecurity/controllers/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.learn.springsecurity.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,12 @@ public class HelloWorldController {
     @GetMapping("hello")
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    // How to gen csrf token
+    // '_csrf' is from the html input property 'name'
+    @GetMapping("csrf-token")
+    public CsrfToken retrieveCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf"); // Cast to CsrfToken (Object)
     }
 }
